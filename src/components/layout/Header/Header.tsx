dc62f88af9438dab16f323d95c206b9dcd1bab8a
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-06-08 21:51:57 -0300

    feat: Header scss rules

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useLayoutEffect, useRef } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 //Contexts
@@ -6,6 +6,9 @@ import { WeatherContext } from "../../../contexts/WeatherContext";
 
 //Components
 import MetricToggleSwitch from "../../common/MetricToggleSwitch/MetricToggleSwitch";
+
+//Icons
+import { FaSearch } from "react-icons/fa";
 
 //Styles
 import styles from "./Header.module.scss";
@@ -15,6 +18,8 @@ type Inputs = {
 };
 
 const Header = () => {
+    const formRef = useRef<HTMLFormElement>(null);
+
     const { setQuery } = useContext(WeatherContext);
 
     const { register, handleSubmit, resetField } = useForm<Inputs>();
@@ -24,10 +29,30 @@ const Header = () => {
         setQuery(data.locationQuery);
         resetField("locationQuery");
     }
+
+    useLayoutEffect(() => {
+        formRef.current?.querySelectorAll("input").forEach((element) => {
+            element.addEventListener("blur", (event) => {
+                const target = event.target as HTMLInputElement;
+
+                if (target.value != "") {
+                    target.nextElementSibling?.classList.add("filled");
+                } else {
+                    target.nextElementSibling?.classList.remove("filled");
+                }
+            });
+        });
+    }, []);
+
     return (
         <header className={styles.headerContainer}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" placeholder="Buscar cidade" {...register("locationQuery", { required: true })} />
+            <form onSubmit={handleSubmit(onSubmit)} ref={formRef}>
+                <div className={styles.inputContainer}>
+                    <input type="text" {...register("locationQuery", { required: true })} />
+                    <label htmlFor="location">
+                        <FaSearch /> Buscar localização
+                    </label>
+                </div>
             </form>
 
             <MetricToggleSwitch />
